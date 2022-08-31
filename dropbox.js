84e37e3ab8073a09258d3fac9236051c4bb12e30
lobau <lbaumann@me.com>
2022-08-31 01:20:40 -0700

    TODO: make it work

--- dropbox.js
+++ dropbox.js
@@ -1,3 +1,5 @@
+var CLIENT_ID = 'nqh4k19borzokuy';
+
 function loadFile(filePath) {
     var dbx = new Dropbox.Dropbox({
         accessToken: getAccessTokenFromUrl()
@@ -7,9 +9,8 @@ function loadFile(filePath) {
         var blob = response.result.fileBlob;
         var reader = new FileReader();
         reader.addEventListener("loadend", function () {
-            document.getElementById("math").value = reader.result;
-            magic2math();
-            // console.log(reader.result); // will print out file content
+            window.editor.setValue(reader.result);
+            render();
         });
         reader.readAsText(blob);
     }).catch(function (error) {
@@ -21,12 +22,12 @@ function newFile(filename) {
     var dbx = new Dropbox.Dropbox({
         accessToken: getAccessTokenFromUrl()
     });
+    // TODO: better template than 40 + 2
     let file = new File(["40 + 2"], filename, { type: "text/plain" });
 
     dbx.filesUpload({ path: "/" + file.name, contents: file })
         .then(function (response) {
-            console.log(response);
-
+            // console.log(response);
             // Update the URL parameters
             const searchParams = new URLSearchParams(location.search);
             searchParams.set('selectedFile', filename);
@@ -39,104 +40,33 @@ function newFile(filename) {
 
 function saveFile() {
     const UPLOAD_FILE_SIZE_LIMIT = 150 * 1024 * 1024;
-    const textContent = document.getElementById("math").value;
+    const textContent = window.editor.getValue();
     const selectedFile = new URLSearchParams(window.location.search).get('selectedFile');
 
     if (selectedFile && textContent) {
         var dbx = new Dropbox.Dropbox({
             accessToken: getAccessTokenFromUrl()
         });
-
-        console.log(textContent);
-
         let file = new File([textContent], selectedFile, { type: "text/plain" });
-
         document.getElementById('save').className = "saving";
 
         if (file.size < UPLOAD_FILE_SIZE_LIMIT) {
-            // File is smaller than 150 Mb - use filesUpload API
             dbx
                 .filesUpload({ path: "/" + file.name, contents: file, mode: 'overwrite', mute: true })
                 .then(function (response) {
-                    // document.getElementById('save').textContent = "Saved!";
                     document.getElementById('save').className = "saved";
                     setTimeout(function () {
                         document.getElementById('save').className = "default";
-                        // document.getElementById('save').textContent = "Save";
                     }, 3000);
-                    // console.log(response);
                 })
                 .catch(function (error) {
                     document.getElementById('save').className = "error";
-                    // document.getElementById('save').textContent = "Error!";
                     setTimeout(function () {
                         document.getElementById('save').className = "default";
-                        // document.getElementById('save').textContent = "Save";
                     }, 3000);
                     console.error(error);
                 });
         }
-        // else {
-        //     // File is bigger than 150 Mb - use filesUploadSession* API
-        //     const maxBlob = 8 * 1000 * 1000; // 8Mb - Dropbox JavaScript API suggested max file / chunk size
-
-        //     var workItems = [];
-
-        //     var offset = 0;
-
-        //     while (offset < file.size) {
-        //         var chunkSize = Math.min(maxBlob, file.size - offset);
-        //         workItems.push(file.slice(offset, offset + chunkSize));
-        //         offset += chunkSize;
-        //     }
-
-        //     const task = workItems.reduce((acc, blob, idx, items) => {
-        //         if (idx == 0) {
-        //             // Starting multipart upload of file
-        //             return acc.then(function () {
-        //                 return dbx
-        //                     .filesUploadSessionStart({ close: false, contents: blob })
-        //                     .then((response) => response.session_id);
-        //             });
-        //         } else if (idx < items.length - 1) {
-        //             // Append part to the upload session
-        //             return acc.then(function (sessionId) {
-        //                 var cursor = { session_id: sessionId, offset: idx * maxBlob };
-        //                 return dbx
-        //                     .filesUploadSessionAppendV2({
-        //                         cursor: cursor,
-        //                         close: false,
-        //                         contents: blob
-        //                     })
-        //                     .then(() => sessionId);
-        //             });
-        //         } else {
-        //             // Last chunk of data, close session
-        //             return acc.then(function (sessionId) {
-        //                 var cursor = { session_id: sessionId, offset: file.size - blob.size };
-        //                 var commit = {
-        //                     path: "/" + file.name,
-        //                     mode: "add",
-        //                     autorename: true,
-        //                     mute: false
-        //                 };
-        //                 return dbx.filesUploadSessionFinish({
-        //                     cursor: cursor,
-        //                     commit: commit,
-        //                     contents: blob
-        //                 });
-        //             });
-        //         }
-        //     }, Promise.resolve());
-
-        //     task
-        //         .then(function (result) {
-        //             console.log("File uploaded");
-        //         })
-        //         .catch(function (error) {
-        //             console.error(error);
-        //         });
-        // }
     }
 
 
---
+++ script.js
@@ -0,0 +1,90 @@
+function getAccessTokenFromUrl() {
+    const searchParams = new URLSearchParams(location.search);
+    return searchParams.get('access_token');
+}
+
+function isAuthenticated() {
+    return !!getAccessTokenFromUrl();
+}
+
+
+function renderItems(items) {
+    var filesContainer = document.getElementById('filelist');
+    const selectedFile = new URLSearchParams(window.location.search).get('selectedFile');
+
+    items.forEach(function (item) {
+        var button = document.createElement('button');
+        button.textContent = "📄 " + item.name;
+        if (selectedFile == item.name) {
+            button.classList.add("selected");
+        }
+        button.addEventListener("click", function () {
+            loadFile(item.name);
+
+            // Update the URL parameters
+            const searchParams = new URLSearchParams(location.search);
+            searchParams.set('selectedFile', item.name);
+            location.search = searchParams.toString();
+        });
+        filesContainer.appendChild(button);
+    });
+
+    var newFileButton = document.createElement('button');
+    newFileButton.textContent = "➕ New sheet";
+    newFileButton.addEventListener("click", function () {
+        let epoch = new Date().valueOf();
+        newFile(epoch);
+    });
+    filesContainer.appendChild(newFileButton);
+}
+
+function showPageSection(elementId) {
+    document.getElementById(elementId).style.display = 'block';
+}
+
+if (isAuthenticated()) {
+    showPageSection('authed-section');
+
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const selectedFile = urlParams.get('selectedFile');
+
+    window.editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
+        mode: "markdown",
+        theme: "uxmap",
+        lineNumbers: false,
+        lineWrapping: true
+    });
+    window.editor.on('change', function (i, op) {
+        localStorage.setItem("markdown", window.editor.getValue());
+        render();
+    });
+
+    if (selectedFile) {
+        loadFile(selectedFile);
+
+    } else {
+        // load the first item?
+        // onboarding?
+    }
+
+    var dbx = new Dropbox.Dropbox({ accessToken: getAccessTokenFromUrl() });
+    dbx.filesListFolder({ path: '' })
+        .then(function (response) {
+            renderItems(response.result.entries);
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
+} else {
+    showPageSection('pre-auth-section');
+
+    // Set the login anchors href using dbx.getAuthenticationUrl()
+    var dbx = new Dropbox.Dropbox({ clientId: CLIENT_ID });
+    var authUrl = dbx.auth.getAuthenticationUrl('https://lobau.io/ucalc/')
+        .then((authUrl) => {
+            document.getElementById('authlink').addEventListener('click', () => {
+                window.location = authUrl;
+            });
+        })
+}

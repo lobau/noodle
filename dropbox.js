995fa7d6690e20d1b6dfb636c239b3bd84f648c8
lobau <lbaumann@me.com>
2022-09-05 15:31:31 -0700

    lotsa progress

--- dropbox.js
+++ dropbox.js
@@ -1,13 +1,29 @@
 var CLIENT_ID = 'nqh4k19borzokuy';
 
+function highlightSelected() {
+    const selectedFile = new URLSearchParams(window.location.search).get('selectedFile');
+
+    for (var i = 0; i < window.buttons.length; i++) {
+        window.buttons[i].classList.remove("loading");
+        if (window.buttons[i].dataset.filename == selectedFile) {
+            window.buttons[i].classList.add("selected");
+        } else {
+            window.buttons[i].classList.remove("selected");
+        }
+    }
+}
+
 function loadFile(filePath) {
     var dbx = new Dropbox.Dropbox({
-        accessToken: getAccessTokenFromUrl()
+        // accessToken: getAccessTokenFromUrl(),
+        accessToken: window.access_token
     });
 
     dbx.filesDownload({ path: '/' + filePath }).then(function (response) {
         var blob = response.result.fileBlob;
         var reader = new FileReader();
+        console.log(filePath);
+        highlightSelected();
         reader.addEventListener("loadend", function () {
             window.editor.setValue(reader.result);
             render();
@@ -20,10 +36,43 @@ function loadFile(filePath) {
 
 function newFile(filename) {
     var dbx = new Dropbox.Dropbox({
-        accessToken: getAccessTokenFromUrl()
+        // accessToken: getAccessTokenFromUrl(),
+        accessToken: window.access_token
     });
     // TODO: better template than 40 + 2
-    let file = new File(["40 + 2"], filename, { type: "text/plain" });
+    const fileTemplate = `# Sundown cheat cheet
+Sundown is just plain Markdown, but with a twist. It can calculate things inline.
+Imagine you want to calculate the price of a party, but it depends on a **lot** of factors, like the number of people showing up, the amount of food to order, etc. 
+You might want to make a quick model. Let setup some variables:
+
+## The pizza
+{{
+pizza_count = 5
+price_per_pizza = 19
+total_pizza_price = pizza_count * price_per_pizza
+}}
+
+## Price per guest
+{{
+guests_count = 10
+pizza_per_guest = pizza_count / guests_count
+price_per_guest = price_per_pizza * pizza_per_guest
+}}
+
+> To recap, we will have { guests_count } guests over, and we will order { pizza_count } pizza, for a total of \${ total_pizza_price } or \${ price_per_guest } per guest.
+
+## Some random things it can do
+{{
+log(23) 
+23 % of 1023 
+200 sec + 120 % 
+30 minutes + 34 day in sec 
+cos(PI) 
+speed = 27 kph 
+speed in mps  
+456 as hex
+}}`
+    let file = new File([fileTemplate], filename, { type: "text/plain" });
 
     dbx.filesUpload({ path: "/" + file.name, contents: file })
         .then(function (response) {
@@ -45,7 +94,8 @@ function saveFile() {
 
     if (selectedFile && textContent) {
         var dbx = new Dropbox.Dropbox({
-            accessToken: getAccessTokenFromUrl()
+            // accessToken: getAccessTokenFromUrl(),
+            accessToken: window.access_token
         });
         let file = new File([textContent], selectedFile, { type: "text/plain" });
         document.getElementById('save').className = "saving";
--- script.js
+++ script.js
@@ -1,14 +1,44 @@
-// function getAccessTokenFromUrl() {
-//     const searchParams = new URLSearchParams(location.search);
-//     return searchParams.get('access_token');
-// }
-
-function getAccessTokenFromUrl() {
-    return utils.parseQueryString(window.location.hash).access_token;
+const setCookie = (name, value, hours = 4, path = '/') => {
+    const expires = new Date(Date.now() + hours * 144e5).toUTCString();
+    document.cookie = name + '=' + encodeURIComponent(value) + '; expires=' + expires + '; path=' + path + ';SameSite=Strict';
 }
 
+const getCookie = (name) => {
+    return document.cookie.split('; ').reduce((r, v) => {
+        const parts = v.split('=');
+        return parts[0] === name ? decodeURIComponent(parts[1]) : r;
+    }, '')
+}
+
+const deleteCookie = (name, path) => {
+    setCookie(name, '', -1, path);
+}
+
+// function getAccessTokenFromUrl() {
+//     return utils.parseQueryString(window.location.hash).access_token;
+// }
+
 function isAuthenticated() {
-    return !!getAccessTokenFromUrl();
+
+    let access_token = utils.parseQueryString(window.location.hash).access_token;
+    if (access_token) {
+        // first, check if there is a hash in the url
+        // if there is, that means the user just logged in and dropbox send us a new access token
+        setCookie("access_token", access_token, 4, "/");
+        window.access_token = access_token;
+        window.history.pushState({ "access_token": access_token }, 'Sundown', '/');
+        return true;
+    } else if (getCookie("access_token")) {
+        // If there is a valid token stored as a cookie, we're good
+        access_token = getCookie("access_token");
+        window.access_token = access_token;
+        return true;
+    } else if (window.access_token) {
+        // If the window object already have a reference to access_token, we're good
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function stringToSeed(str) {
@@ -20,39 +50,59 @@ function stringToSeed(str) {
     return values.join('') + 0;
 }
 
-
 function renderItems(items) {
     var filesContainer = document.getElementById('filelist');
     const selectedFile = new URLSearchParams(window.location.search).get('selectedFile');
 
-    let possible_icons = ["📒", "📓", "📔", "📕", "📗", "📘", "📙"];
-
-    items.forEach(function (item, index) {
-        var button = document.createElement('button');
-        var seed = stringToSeed(item.name) % possible_icons.length;
-        // console.log(item.name, seed, seed % possible_icons.length);
-        button.textContent = possible_icons[seed] + " " + item.name;
-        if (selectedFile == item.name) {
-            button.classList.add("selected");
-        }
-        button.addEventListener("click", function () {
-            loadFile(item.name);
-
-            // Update the URL parameters
-            const searchParams = new URLSearchParams(location.search);
-            searchParams.set('selectedFile', item.name);
-            location.search = searchParams.toString();
-        });
-        filesContainer.appendChild(button);
-    });
-
     var newFileButton = document.createElement('button');
-    newFileButton.textContent = "➕ New sheet";
+    newFileButton.innerHTML = "<span>✨</span><span>Create New Sheet</span>";
     newFileButton.addEventListener("click", function () {
         let epoch = new Date().valueOf();
-        newFile(epoch);
+        let filename = prompt('Filename (default is epoch)', epoch);
+        // let filename = encodeURI(filenamePrompt);
+        // console.log(filename);
+        if(filename) {
+            newFile(filename + ".md");
+        } else {
+            newFile(epoch + ".md");
+        }
+        
     });
     filesContainer.appendChild(newFileButton);
+    filesContainer.appendChild(document.createElement('hr'));
+
+    let possible_icons = ["📒", "📓", "📔", "📕", "📗", "📘", "📙"];
+    window.buttons = [];
+
+    items.forEach(function (item, index) {
+        // is that a markdown file?
+        if (item.name.split(".").reverse()[0] == "md") {
+            var button = document.createElement('button');
+            var seed = stringToSeed(item.name) % possible_icons.length;
+            button.innerHTML = "<span>" + possible_icons[seed] + "</span><span>" + item.name + "</span>";
+            button.dataset.filename = item.name;
+            if (selectedFile == item.name) {
+                button.classList.add("selected");
+            }
+            button.addEventListener("click", function () {
+                loadFile(item.name);
+
+                const searchParams = new URLSearchParams(location.search);
+                searchParams.set('selectedFile', item.name);
+                window.history.pushState({ "access_token": access_token }, 'Sundown', '/?' + searchParams.toString());
+
+                button.classList.add("loading");
+
+                // Update the URL parameters
+                // const searchParams = new URLSearchParams(location.search);
+                // searchParams.set('selectedFile', item.name);
+                // location.search = searchParams.toString();
+            });
+            filesContainer.appendChild(button);
+            window.buttons.push(button);
+        }
+
+    });
 }
 
 function showPageSection(elementId) {
@@ -87,7 +137,7 @@ if (isAuthenticated()) {
         // onboarding?
     }
 
-    var dbx = new Dropbox.Dropbox({ accessToken: getAccessTokenFromUrl() });
+    var dbx = new Dropbox.Dropbox({ accessToken: window.access_token });
     dbx.filesListFolder({ path: '' })
         .then(function (response) {
             renderItems(response.result.entries);
@@ -103,7 +153,7 @@ if (isAuthenticated()) {
     let protocol;
     protocol = (window.location.hostname == "localhost") ? "http" : "https";
     currentHost = protocol + "://" + window.location.host;
-    console.log(currentHost);
+    // console.log(currentHost);
     var authUrl = dbx.auth.getAuthenticationUrl(currentHost)
         .then((authUrl) => {
             document.getElementById('authlink').addEventListener('click', () => {
